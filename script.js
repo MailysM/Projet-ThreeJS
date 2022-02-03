cadda64f1aa5d2c8948b8d36718966a62cfae9f3
MONGE Mailys <mailys.monge@laposte.net>
2022-02-03 18:13:56 +0100

    creation tous les cubes

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const camera = new THREE.PerspectiveCamera( 45,aspect,0.1,100 );
 camera.position.z = 9;
 const renderer = new THREE.WebGLRenderer();
 const controls = new THREE.OrbitControls( camera , renderer.domElement );
+// spotlight
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(200, 400, 300);
+scene.add(spotLight);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -13,20 +17,23 @@ document.body.appendChild( renderer.domElement );
 //cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({
-    color: 0x00ff00,
-    wireframe: true,
+    color: 0x00afaf,
+    side: THREE.DoubleSide
 });
 
 function createMap(size,length,pas){
     let totalLength = size*length+(size-1)*pas;
-    for (let i = 0; i < totalLength; i++) {
-        let cube = new THREE.Mesh(geometry, material);
-        cube.position.x = i;
-        scene.add(cube);
+    for (let i = 0; i < totalLength; i+=length) {
+        for(let j = 0; j < totalLength; j+=length){
+            let cube = new THREE.Mesh(geometry, material);
+            cube.position.x = i + i*pas;
+            cube.position.y = j + j*pas;
+            scene.add(cube);
+        }
     }
 }
 
-createMap(10);
+createMap(4,1,0.5);
 
 
 
