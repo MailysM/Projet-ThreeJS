60e0ff28d933133742891bf8475f84e610fefd0b
MONGE Mailys <mailys.monge@laposte.net>
2022-02-04 08:39:26 +0100

    changement script

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Variables Jeux
+const size = 4;
+const length = 1;
+const pas = 0.5;
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
@@ -5,13 +10,13 @@ const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera( 70,aspect,0.1, 100 );
 
 camera.position.z = 9;
-camera.rotation.set(0.5,0,0);
 
 const renderer = new THREE.WebGLRenderer();
 const controls = new THREE.OrbitControls( camera , renderer.domElement );
 controls.enablePan = true;
 controls.enableZoom = true;
-controls.enableRotate = false;
+controls.enableRotate = true;
+controls.enableK
 
 controls.keys = {
 	LEFT: 'ArrowLeft', //left arrow
@@ -19,6 +24,16 @@ controls.keys = {
 	RIGHT: 'ArrowRight', // right arrow
 	BOTTOM: 'ArrowDown' // down arrow
 }
+controls.listenToKeyEvents(window);
+
+controls.mouseButtons = {
+	LEFT: null,
+	MIDDLE: THREE.MOUSE.DOLLY,
+	RIGHT: null
+}
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = true;
 
 controls.minPolarAngle = 2.35;
 controls.maxPolarAngle = 2.35;
@@ -40,56 +55,71 @@ document.body.appendChild( renderer.domElement );
 //cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({
-    color: 0x00afaf,
+    color:  0xFFC300  ,
     side: THREE.DoubleSide
 });
+
+//plateau
+const sizePlateau = size*length+(size-1)*pas+2*pas;
+const materialPlateau = new THREE.MeshLambertMaterial({
+    color:  0x581845 ,
+    side: THREE.DoubleSide
+});
+const geometryRect = new THREE.BoxGeometry(sizePlateau,sizePlateau,0.1);
+
+
+let cubes = [];
 
 function createMap(size,length,pas){
     let totalLength = size*length+(size-1)*pas;
     for (let i = 0; i < size; i+=length) {
         for(let j = 0; j < size; j+=length){
-            let cube = new THREE.Mesh(geometry, material);
-            cube.position.x = i + i*pas;
-            cube.position.y = j + j*pas;
-            scene.add(cube);
+            cubes[i,j] = new THREE.Mesh(geometry, material);
+            cubes[i,j].position.x =  i + i*pas - Math.floor(totalLength/2);
+            cubes[i,j].position.y = j + j*pas;
+            // cubes[i,j].addEventListener( 'mouseover', onPointerMove );
+            scene.add(cubes[i,j]);
         }
     }
-    camera.position.x = Math.floor(totalLength/2);
-    camera.position.y = Math.floor(totalLength/2);
-}
-
-function onPointerMove( event ) {
-
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-
+    let rectangle = new THREE.Mesh(geometryRect,materialPlateau);
+    rectangle.position.x = pas -pas/3;
+    rectangle.position.y = pas + Math.floor(totalLength/2);
+    rectangle.position.z = -0.5;
+    scene.add(rectangle);
 
 }
+
+// function onPointerMove( event ) {
+
+// 	// calculate pointer position in normalized device coordinates
+// 	// (-1 to +1) for both components
+//     console.log("ok");
+// 	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+// 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+
+
+// }
 
 ///Main
 
-createMap(4,1,0.5);
+createMap(size,length,pas);
 
-document.addEventListener( 'pointermove', onPointerMove );
 
 const render = function () {
     requestAnimationFrame( render );
 
-    // update the picking ray with the camera and pointer position
-	raycaster.setFromCamera( pointer, camera );
+    // // update the picking ray with the camera and pointer position
+	// raycaster.setFromCamera( pointer, camera );
 
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children );
+	// // calculate objects intersecting the picking ray
+	// const intersects = raycaster.intersectObjects( scene.children );
 
-    for ( let i = 0; i < intersects.length; i ++ ) {
+    // for ( let i = 0; i < intersects.length; i ++ ) {
 
-		intersects[ i ].object.material.color.set( 0xff0000 );
+	// 	intersects[ i ].object.material.color.set( 0xff0000 );
 
-	}
+	// }
     controls.update();
 
     renderer.render(scene, camera);
